4df5475e1e2587676d52cd1d28a9c75966c4f3a0
silvioaburto <sjortiz@ucdavis.edu>
2020-05-06 17:54:45 -0700

    Updates

---
+++ precache-manifest.d2228eab336898de76274327b73ef245.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec80d9102f463c71028b22fcfa16bd6",
+    "url": "/covid_19/index.html"
+  },
+  {
+    "revision": "fc574b6846b1c444a220",
+    "url": "/covid_19/static/css/main.888adf22.chunk.css"
+  },
+  {
+    "revision": "b6d4dff8e30760a8574f",
+    "url": "/covid_19/static/js/2.a20fe546.chunk.js"
+  },
+  {
+    "revision": "278be41389e3536a5347a9018737729d",
+    "url": "/covid_19/static/js/2.a20fe546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc574b6846b1c444a220",
+    "url": "/covid_19/static/js/main.0776874c.chunk.js"
+  },
+  {
+    "revision": "4376d0aa09b71857261e",
+    "url": "/covid_19/static/js/runtime-main.6e362de2.js"
+  }
+]);
